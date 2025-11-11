b24c5c403781ad1fffe41880c7f44f1dfa1e6131
thayanne204 <oliveirathayanne438@gmail.com>
2025-11-11 20:55:18 -0300

    Create no.java

---
+++ no.java
@@ -0,0 +1,13 @@
+package arvorebinariar;
+
+public class no {
+    int valor;
+    no esquerda;
+    no direita;
+
+    public no (int valor) {
+        this.valor = valor;
+        this.esquerda = null;
+        this.direita = null;
+    }
+}
